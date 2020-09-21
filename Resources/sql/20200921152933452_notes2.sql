c2ac9207958bb51d7c0493c81c41b5efe1d51143
jenkins <jenkins@datical.com>
2020-09-21 15:30:10 -0400

    Datical DB Deployment Packager   Deployment Pipeline: current   Deploy / Rollback Scripts Processed (Associated Labels): sql_direct/notes2.sql / none (current,notes2.sql)

---
+++ Resources/sql/20200921152933452_notes2.sql
@@ -0,0 +1,19 @@
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE NOTES';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
+
+create table notes (id number(38), note varchar(200) );
+insert into notes values (1,'Hello World');
+insert into notes values (2,'Hey There!');
+insert into notes values (3,'Hi Bye');
+insert into notes values (4,'Copy-n-Paste');
+insert into notes values (5,'More notes');
+commit;
+
+alter table employees add (state varchar2(30));
